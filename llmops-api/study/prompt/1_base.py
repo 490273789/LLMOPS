c63f7ecd74620831528d63d750041a98bb9b6a2c
Ethan Wang <ethan.wang1993@outlook.com>
2025-11-08 14:34:48 +0800

    feat: Prompt module

---
+++ llmops-api/study/prompt/1_base.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from langchain_core.prompts import (
+    PromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+)
+from langchain_core.messages import AIMessage
+
+prompt = PromptTemplate.from_template("请讲一个关于{subject}的冷笑话")
+print("PromptTemplate:", prompt)
+print("PromptTemplate.format:", prompt.format(subject="程序员"))
+
+prompt_value = prompt.invoke({"subject": "程序员"})
+print("PromptValue:", prompt_value)
+print("PromptValue.to_string:", prompt_value.to_string())
+print("PromptValue.to_messages:", prompt_value.to_messages())
+
+# 创建一个 ChatPromptTemplate
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你是我的私人助理，根据我的提问进行回复，当前时间为{now}"),
+        #  消息占位符, 可能有其他的消息，但是不确定
+        MessagesPlaceholder("chat_history"),
+        HumanMessagePromptTemplate.from_template("请讲一个关于{subject}的冷笑话"),
+    ]
+).partial(now=datetime.now())
+
+chat_prompt_value = chat_prompt.invoke(
+    {
+        "chat_history": [
+            ("human", "我叫小王"),
+            AIMessage("你好，我是小助理，有什么可以帮到你的！"),
+        ],
+        "subject": "程序员",
+    }
+)
+print()
+print("chat_prompt_value:", chat_prompt_value)
+print("chat_prompt_value to str:", chat_prompt_value.to_string())
